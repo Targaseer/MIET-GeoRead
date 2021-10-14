042693410073a7b67ebf89b8fea47bc98fdea362
Kokurcho <41186729+Kokurcho@users.noreply.github.com>
2021-10-14 10:55:38 +0300

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,11 @@
+#include iostream
+using namespace std;
+int main() {
+  char name[20];
+  cout  Enter your name ;
+  cin  name;
+  cout  Hello,   name  !n;
+  cout  Nice to see you learning Gitn;
+  system(pause);
+  return 0;
+}
