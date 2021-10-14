fe404b8ed43f4b8a2ddc5b98d31077eb60914397
Kokurcho <41186729+Kokurcho@users.noreply.github.com>
2021-10-14 12:02:37 +0300

    Add files via upload
    Добавил класс DataBaseConnector. Накидал шапки методов без реализаций. Немного не понял, что делать с полями

---
+++ DataBaseConnector.java
@@ -0,0 +1,16 @@
+public class DataBaseConnector
+{
+    // я хз чё делать с полями, как правильно задать класс без полей
+    // это интерфейс?
+    
+    public boolean saveBook (Book book)
+    {
+        //сохранение книги в бд
+    }
+    
+    public boolean saveLink (Link link)
+    {
+        //сохранение ссылки в бд
+    }
+    
+}
